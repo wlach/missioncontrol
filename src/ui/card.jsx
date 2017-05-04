6339c71c37a82553e20edf6f951da9ef4d785487
William Lachance <wlachance@mozilla.com>
2017-05-04 16:07:18 -0400

    experiments with using victory charts

--- src/ui/card.jsx
+++ src/ui/card.jsx
@@ -1,7 +1,45 @@
+import { VictoryAxis, VictoryArea, VictoryChart, VictoryStack, VictoryTheme, VictoryTooltip } from 'victory';
 import MetricsGraphics from 'react-metrics-graphics';
 import React from 'react';
 import { timeFormat } from 'd3-time-format';
 import { Card, Modal, ModalHeader, ModalBody } from 'reactstrap';
+
+class MeasureGraph extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: this.props.name,
+      data: this.props.data,
+      width: this.props.width,
+      height: this.props.height,
+    };
+  }
+
+  render() {
+    return (
+      <VictoryChart
+        width={this.state.width}
+        height={this.state.height}
+        theme={VictoryTheme.material}>
+        <VictoryAxis tickFormat={secs => `${new Date(secs).getHours()}h`} />
+        <VictoryAxis dependentAxis/>
+        <VictoryStack>
+          {
+            this.state.data.map(d => (
+              <VictoryArea
+                labels={v => v.main_rate}
+                labelComponent={<VictoryTooltip/>}
+                data={d}
+                x="date"
+                y="main_rate"
+                />
+            ))
+          }
+        </VictoryStack>
+      </VictoryChart>
+    );
+  }
+}
 
 export default class DataCard extends React.Component {
   constructor(props) {
@@ -30,17 +68,7 @@ export default class DataCard extends React.Component {
       <Card block onClick={this.toggleDetail}>
         <center><h4>{this.state.name}</h4></center>
         <figure className="graph" id={this.state.name}>
-          <MetricsGraphics
-            data={this.state.data}
-            width={280}
-            area={true}
-            height={200}
-            x_accessor="date"
-            y_accessor="main_rate"
-            xax_format={timeFormat('%Hh')}
-            xax_count={4}
-            right={40}
-            />
+          <MeasureGraph width={280} height={200} name={this.state.name} data={this.state.data}/>
         </figure>
         <Modal isOpen={this.state.showDetail} size="lg" toggle={this.toggleDetail}>
           <ModalHeader toggle={this.toggleDetail}>{this.state.name}</ModalHeader>
@@ -49,16 +77,9 @@ export default class DataCard extends React.Component {
               <div className="row">
                 <div className="col">
                   <center><h5>Crash rate</h5></center>
-                  <MetricsGraphics
-                    data={this.state.data}
-                    width={700}
-                    height={300}
-                    x_accessor="date"
-                    y_accessor="main_rate"
-                    xax_format={timeFormat('%Hh')}
-                    linked={true}
-                    linked_format="%Y-%m-%d-%H-%M-%S"
-                    />
+                  <MeasureGraph
+                    width={700} height={300} name={this.state.name}
+                    data={this.state.data}/>
                 </div>
               </div>
               <div className="row">
